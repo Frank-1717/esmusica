e03a75bda8621fce4ff77b1101ae5f90b3069be1
Francisco Riveros <franciscoriverospalacios@gmail.com>
2021-04-06 22:01:37 -0400

    build

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,7 @@ gulp.task("licenses", async function() {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
-* Now UI Kit PRO React - v1.0.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-pro-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */`)
     )
@@ -31,18 +20,7 @@ gulp.task("licenses", async function() {
     .pipe(
       gap.prependText(`<!--
 
-=========================================================
-* Now UI Kit PRO React - v1.0.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-pro-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 -->`)
     )
@@ -54,18 +32,7 @@ gulp.task("licenses", async function() {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
-* Now UI Kit PRO React - v1.0.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-pro-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */`)
     )
